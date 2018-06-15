26a35217d31fc8c17045b3a9ffc0c105ba259ae5
Magdalena Tuszynska <tuszynska.m@gmail.com>
2018-06-16 00:49:02 +0200

    file with arrays and boolean added

---
+++ zad02.py
@@ -0,0 +1,4 @@
+tablica = [1, 4, 12, "elo"]
+tablica.append(123)
+tablica[0] = tablica[2]
+print(tablica)
---
+++ zad03.py
@@ -0,0 +1,6 @@
+boo1 = True
+boo2 = False
+print(boo1 or boo2)
+print(boo1 and boo2)
+print(not boo1)
+print(not boo2)
