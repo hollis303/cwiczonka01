d1f21e5700016f989a06f5a74110e172f9602113
Magdalena Tuszynska <tuszynska.m@gmail.com>
2018-06-16 00:28:57 +0200

    template added

--- zad01.py
+++ zad01.py
@@ -3,3 +3,5 @@ pozegnanie="goodbye world"
 print(powitanie)
 print(pozegnanie)
 print(powitanie + " " + pozegnanie)
+POWITANIEPOZEGNANIE = "{pow} {poz}"
+print(POWITANIEPOZEGNANIE.format(pow = powitanie, poz = pozegnanie))
